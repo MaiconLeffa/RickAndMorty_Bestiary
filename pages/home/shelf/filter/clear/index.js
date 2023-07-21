eb35274af6a8ca4380056d9ed687081199e795f9
Maicon <maicon.leffa@eumedicoresidente.com.br>
2023-07-21 09:51:26 -0300

    criando const

--- pages/home/shelf/filter/clear/index.js
+++ pages/home/shelf/filter/clear/index.js
@@ -1,8 +1,12 @@
-import { useContext } from 'react'
-import context from '../../../homeContext/context'
-import styles from './clear.module.css'
+import { useContext } from "react";
+import context from "../../../homeContext/context";
+import styles from "./clear.module.css";
 
-export default () => {
-  const { clearFilters } = useContext(context)
-  return <button className={styles.clear} onClick={() => clearFilters()}>Clear filters</button>
-}
+export default Context = () => {
+  const { clearFilters } = useContext(context);
+  return (
+    <button className={styles.clear} onClick={() => clearFilters()}>
+      Clear filters
+    </button>
+  );
+};
