5baa38d6fbf2b00af383202fc83b16da4a86aaa6
Maicon <maicon.leffa@eumedicoresidente.com.br>
2023-07-21 09:56:37 -0300

    corrigindo export dos componentes

--- elements/head/index.js
+++ elements/head/index.js
@@ -1,8 +1,14 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-export default () =>
+const HeadComp = () => (
   <Head>
     <title>Rick and Morty - Bestiary</title>
-    <meta name="description" content="A list of all creatures you can find in Rick And Morty series" />
+    <meta
+      name="description"
+      content="A list of all creatures you can find in Rick And Morty series"
+    />
     <link rel="icon" href="/favicon.ico" />
   </Head>
+);
+
+export default HeadComp;
--- pages/home/shelf/filter/index.js
+++ pages/home/shelf/filter/index.js
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import Clear from './clear'
-import Name from './name'
-import MobileButton from './mobileButton'
-import Species from './species'
-import Status from './status'
-import styles from './filter.module.css'
+import { useState } from "react";
+import Clear from "./clear";
+import Name from "./name";
+import MobileButton from "./mobileButton";
+import Species from "./species";
+import Status from "./status";
+import styles from "./filter.module.css";
 
-export default () => {
-  const [sideBar, setSideBar] = useState(false)
+const Filter = () => {
+  const [sideBar, setSideBar] = useState(false);
 
-  const toggle = () => setSideBar(!sideBar)
+  const toggle = () => setSideBar(!sideBar);
 
   return (
     <div onClick={toggle} opened={sideBar.toString()} className={styles.filter}>
       <MobileButton visible={!sideBar} onPress={toggle} />
-      <div onClick={e => e.stopPropagation()} className={styles.filter__container}>
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={styles.filter__container}
+      >
         <p className={styles.filter__title}>Filters:</p>
         <Name />
         <Status />
@@ -22,5 +25,7 @@ export default () => {
         <Clear />
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default Filter;
